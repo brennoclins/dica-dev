c6ae39880c1df1f6b9c17baf84e35120e126ddd4
Brenno - BCL-ST <dev@bcl-st.com.br>
2024-05-13 18:27:39 -0300

    feat: installing and configuring dependencies to keep my code clean and organized, installing libs necessary for the blog to function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <main className="relative flex h-screen flex-col items-center justify-around bg-[url('/ferrari.jpeg')] bg-cover">
       <div className="absolute bg-gray-950/50 w-screen h-screen" />
-      <h1 className="z-50 text-center text-5xl text-white font-extrabold">Dica Dev</h1>
-
+      <h1 className="z-50 text-center text-5xl text-white font-extrabold">
+        Dica Dev
+      </h1>
     </main>
-  );
+  )
 }
